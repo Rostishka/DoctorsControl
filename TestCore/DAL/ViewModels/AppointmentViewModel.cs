19437a469082d3d218ea8ca10ab039e934ce63bf
Diakiv Rostyslav <rostyslav.diakiv@gmail.com>
2017-11-11 23:48:59 +0200

    Added Appointments Controller

---
+++ TestCore/DAL/ViewModels/AppointmentViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DAL.Entity;
+
+namespace DAL.ViewModels
+{
+    public class AppointmentViewModel
+    {
+        public DateTime Time { get; set; }
+        public string Comment { get; set; }
+        public DoctorEntity Doctor { get; set; }
+        public int DoctorId { get; set; }
+        public PatientEntity Patient { get; set; }
+        public int PatientId { get; set; }
+    }
+}
